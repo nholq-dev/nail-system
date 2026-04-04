7655e42e2b440a2131d1e8128ccc97a6cf267e9e
nholq-dev <quangnho97@gmail.com>
2026-04-04 16:16:07 +0700

    new

---
+++ src/main/java/com/tkrity/NailChain/NailChainApplication.java
@@ -0,0 +1,13 @@
+package com.tkrity.NailChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailChainApplication.class, args);
+	}
+
+}
